aaf840e1ef1181992c190b2bfb44d597997e5760
Gourav Jamdhade <jamgou123@gmail.com>
2025-02-22 17:36:20 +0530

    models

---
+++ app/src/main/java/com/example/echo/model/ThreadModel.kt
@@ -0,0 +1,10 @@
+package com.example.echo.model
+
+data class ThreadModel(
+
+    val userId: String? = "",
+    val thread: String = "",
+    val imageUrl: String? = "",
+    val timestamp: String = ""
+)
+
